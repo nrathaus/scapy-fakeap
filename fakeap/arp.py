a305397dc9c1a516eb36ca8a3bf12b2869c2f1fd
Noam Rathaus <rathaus@gmail.com>
2024-08-29 16:17:12 +0300

    Too verbose

--- fakeap/arp.py
+++ fakeap/arp.py
@@ -23,7 +23,7 @@ class ARPHandler:
             temp = self.arp_table[client_ip]
         except KeyError:
             temp = None
-            printd(f"Could not find IP {client_ip} in ARP table.", Level.WARNING)
+            # printd(f"Could not find IP {client_ip} in ARP table.", Level.WARNING)
         self.mutex.release()
 
         return temp
